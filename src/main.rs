27aa893ff24a0e71106445e46633b91be5490c9c
Peter Arato <it.arato@gmail.com>
2026-05-31 21:14:02 -0400

    Some prototype

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,104 @@
+mod math;
+
 use bevy::prelude::*;
 
+#[derive(Resource)]
+struct GameState {
+    score: u32,
+}
+
+#[derive(Resource)]
+struct GameMap {
+    path: Vec<Vec2>,
+}
+
 #[derive(Component)]
-struct Person;
+struct Tower;
 
-#[derive(Component, Debug)]
-struct Name(String);
+#[derive(Component)]
+struct Enemy;
 
-fn hello_world() {
-    println!("Hello!");
-}
+#[derive(Resource)]
+struct TowerSpawnTimer(Timer);
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Joe".to_string())));
-    commands.spawn((Person, Name("Pop".to_string())));
-}
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2d);
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("Hello {:?}", name);
+    let shapes = [meshes.add(Circle::new(48.0))];
+
+    for shape in shapes.into_iter() {
+        let color = Color::Srgba(Srgba::new(0.8, 0.2, 0.6, 1.0));
+        commands.spawn((
+            Tower,
+            Mesh2d(shape),
+            MeshMaterial2d(materials.add(color)),
+            Transform::from_xyz(100.0, 200.0, 0.0),
+        ));
     }
 }
 
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Joe" {
-            name.0 = "Joe2".to_string();
-            break;
+fn update_enemy_movement(mut query: Query<&mut Transform, With<Enemy>>, time: Res<Time>) {
+    for mut transform in &mut query {
+        transform.translation.x += 100.0 * time.delta_secs();
+    }
+}
+
+fn update_enemy_spawns(
+    mut commands: Commands,
+    time: Res<Time>,
+    game_map: Res<GameMap>,
+    mut tower_spawn_timer: ResMut<TowerSpawnTimer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    if tower_spawn_timer.0.tick(time.delta()).just_finished() {
+        let shapes = [meshes.add(Circle::new(32.0))];
+        let spawn_point = game_map.path[0];
+
+        for shape in shapes.into_iter() {
+            let color = Color::Srgba(Srgba::new(0.2, 0.8, 0.6, 1.0));
+            commands.spawn((
+                Enemy,
+                Mesh2d(shape),
+                MeshMaterial2d(materials.add(color)),
+                Transform::from_xyz(spawn_point.x, spawn_point.y, 0.0),
+            ));
         }
+    }
+}
+
+struct GamePlugin;
+
+impl Plugin for GamePlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GameMap {
+            path: vec![
+                (-200.0, -200.0).into(),
+                (-200.0, 0.0).into(),
+                (200.0, 0.0).into(),
+                (200.0, 200.0).into(),
+            ],
+        })
+        .insert_resource(TowerSpawnTimer(Timer::from_seconds(1.0, TimerMode::Once)))
+        .add_systems(Startup, setup)
+        .add_systems(Update, (update_enemy_spawns, update_enemy_movement).chain());
     }
 }
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: (1024, 768).into(),
+                title: "Tower Defense".into(),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .add_plugins(GamePlugin)
         .run();
 }
---
+++ src/math.rs
@@ -0,0 +1,59 @@
+use std::ops::Sub;
+
+use bevy::math::Vec2;
+
+fn slope(lhs: Vec2, rhs: Vec2) -> f32 {
+    (lhs.x - rhs.x) / (lhs.y - rhs.y)
+}
+
+fn between(p: f32, lhs: f32, rhs: f32) -> bool {
+    let min = lhs.min(rhs);
+    let max = lhs.max(rhs);
+    min <= p && p <= max
+}
+
+fn point_on_path(p: Vec2, path: (Vec2, Vec2)) -> bool {
+    assert_ne!(path.0, path.1);
+
+    if p.abs_diff_eq(path.0, 0.1) || p.abs_diff_eq(path.1, 0.1) {
+        return true;
+    }
+
+    let main_slope = slope(path.0, path.1);
+    let sub_slope = slope(path.0, p);
+
+    between(p.x, path.0.x, path.1.x) && between(p.y, path.0.y, path.1.y)
+}
+
+pub(crate) fn calculate_next_position_on_path(
+    path: &Vec<Vec2>,
+    x: f32,
+    y: f32,
+    time_delta_secs: f32,
+    speed: f32,
+) -> Vec2 {
+    for i in 0..path.len() {
+        let rev_i = path.len() - 1 - i;
+    }
+
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::math::point_on_path;
+
+    #[test]
+    fn test_point_on_path() {
+        let path = ((1.0, -8.0).into(), (-2.0, -2.0).into());
+
+        assert!(point_on_path((1.0, -8.0).into(), path));
+        assert!(point_on_path((-2.0, -2.0).into(), path));
+
+        assert!(point_on_path((-1.0, -4.0).into(), path));
+        assert!(point_on_path((-1.5, -3.0).into(), path));
+
+        assert!(!point_on_path((-3.0, -0.0).into(), path));
+        assert!(!point_on_path((1.0, 4.0).into(), path));
+    }
+}
