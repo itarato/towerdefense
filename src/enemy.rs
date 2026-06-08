63f1807b24a61a6479c61c14e486aef2726a8a0a
Peter Arato <it.arato@gmail.com>
2026-06-07 21:43:22 -0400

    Wave

--- src/enemy.rs
+++ src/enemy.rs
@@ -2,6 +2,19 @@ use bevy::prelude::*;
 
 pub(crate) const ENEMY_SIZE_RADIUS: f32 = 8.0;
 
+pub(crate) struct Burst {
+    pub(crate) kind: u8,
+    pub(crate) count: u8,
+    pub(crate) delay: f32,
+    pub(crate) frequency: f32,
+}
+
+pub(crate) struct Wave {
+    pub(crate) bursts: Vec<Burst>,
+}
+
+pub(crate) struct Waves(pub(crate) Vec<Wave>);
+
 #[derive(Component)]
 pub(crate) struct Enemy;
 
